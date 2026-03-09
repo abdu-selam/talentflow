5f286f9fb386f6c2e894a353df8c8cc72e267032
Abduselam Awel <abdselam676@gmail.com>
2026-03-09 05:12:57 -0900

    hero section has been styled

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,37 @@
-const navBar = document.querySelector('.header__nav')
-const menu = document.querySelector('.menu')
-const menuIcons = document.querySelectorAll('.menu-icon')
-const navItems = navBar.querySelectorAll('ul, li,a')
+const navBar = document.querySelector(".header__nav");
+const menu = document.querySelector(".menu");
+const menuIcons = document.querySelectorAll(".menu-icon");
+const navItems = navBar.querySelectorAll("ul, li,a");
 
-menu.addEventListener('click',(e)=>{
-    menu.classList.toggle('active')
-    navBar.classList.toggle('active')
-})
+const heroImages = document.querySelectorAll(".hero__back img");
 
-window.addEventListener('click',(e)=>{
-    const elem = e.target
-    if (![...navItems, ...menuIcons, menu, navBar].includes(elem)) {
-        menu.classList.remove('active')
-        navBar.classList.remove('active')
+menu.addEventListener("click", (e) => {
+  menu.classList.toggle("active");
+  navBar.classList.toggle("active");
+});
+
+window.addEventListener("click", (e) => {
+  const elem = e.target;
+  if (![...navItems, ...menuIcons, menu, navBar].includes(elem)) {
+    menu.classList.remove("active");
+    navBar.classList.remove("active");
+  }
+});
+
+let curr = 0;
+
+setInterval(() => {
+  heroImages.forEach((img, i) => {
+    if (i == curr) {
+      img.classList.add("active");
+    } else {
+      img.classList.remove("active");
     }
-})
+  });
+
+  if (curr == 4) {
+    curr = 0;
+  } else {
+    curr++;
+  }
+}, 3000);
