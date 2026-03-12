9926e89c7b9916ab79f0414657039ff6298a0566
Abduselam Awel <abdselam676@gmail.com>
2026-03-12 01:25:15 -0900

    loading animation has been added

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,81 @@
-const navBar = document.querySelector(".header__nav");
-const menu = document.querySelector(".menu");
-const menuIcons = document.querySelectorAll(".menu-icon");
-const navItems = navBar.querySelectorAll("ul, li,a");
+window.addEventListener("load", () => {
+  const loading = document.querySelector('.loading')
+  loading.classList.add('close')
 
-const heroImages = document.querySelectorAll(".hero__back img");
-
-const faqs_item = document.querySelectorAll(".faqs__item");
-const faqs_close = document.querySelectorAll(".faqs__icon");
-
-const containers = document.querySelectorAll(".faqs__container");
-const faqsBtns = document.querySelectorAll(".faqs__btn");
-const faqsBtnsWrapper = document.querySelector(".faqs__btns");
-
-document.querySelector(".footer__year").textContent = new Date().getFullYear();
-
-menu.addEventListener("click", (e) => {
-  menu.classList.toggle("active");
-  navBar.classList.toggle("active");
+  setTimeout(() => {
+    loading.style.display = 'none'
+    init()
+  }, 500);
 });
 
-window.addEventListener("click", (e) => {
-  const elem = e.target;
-  if (![...navItems, ...menuIcons, menu, navBar].includes(elem)) {
-    menu.classList.remove("active");
-    navBar.classList.remove("active");
-  }
-});
+const init = () => {
+  document.getElementById('app').style.display = 'block'
 
-window.addEventListener("scroll", () => {
-  menu.classList.remove("active");
-  navBar.classList.remove("active");
-});
+  const navBar = document.querySelector(".header__nav");
+  const menu = document.querySelector(".menu");
+  const menuIcons = document.querySelectorAll(".menu-icon");
+  const navItems = navBar.querySelectorAll("ul, li,a");
 
-let curr = 0;
+  const heroImages = document.querySelectorAll(".hero__back img");
 
-setInterval(() => {
-  heroImages.forEach((img, i) => {
-    if (i == curr) {
-      img.classList.add("active");
-    } else {
-      img.classList.remove("active");
+  const faqs_item = document.querySelectorAll(".faqs__item");
+  const faqs_close = document.querySelectorAll(".faqs__icon");
+
+  const containers = document.querySelectorAll(".faqs__container");
+  const faqsBtns = document.querySelectorAll(".faqs__btn");
+  const faqsBtnsWrapper = document.querySelector(".faqs__btns");
+
+  document.querySelector(".footer__year").textContent =
+    new Date().getFullYear();
+
+  menu.addEventListener("click", (e) => {
+    menu.classList.toggle("active");
+    navBar.classList.toggle("active");
+  });
+
+  window.addEventListener("click", (e) => {
+    const elem = e.target;
+    if (![...navItems, ...menuIcons, menu, navBar].includes(elem)) {
+      menu.classList.remove("active");
+      navBar.classList.remove("active");
     }
   });
 
-  if (curr == 4) {
-    curr = 0;
-  } else {
-    curr++;
-  }
-}, 3000);
-
-faqs_close.forEach((faq, i) => {
-  faq.addEventListener("click", (e) => {
-    faqs_item[i].classList.toggle("open");
+  window.addEventListener("scroll", () => {
+    menu.classList.remove("active");
+    navBar.classList.remove("active");
   });
-});
 
-faqsBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    if (btn.classList.contains("active")) return;
-    faqsBtns.forEach((b) => b.classList.toggle("active"));
-    faqsBtnsWrapper.classList.toggle("active");
-    containers.forEach((contain) => contain.classList.toggle("active"));
+  let curr = 0;
+
+  setInterval(() => {
+    heroImages.forEach((img, i) => {
+      if (i == curr) {
+        img.classList.add("active");
+      } else {
+        img.classList.remove("active");
+      }
+    });
+
+    if (curr == 4) {
+      curr = 0;
+    } else {
+      curr++;
+    }
+  }, 3000);
+
+  faqs_close.forEach((faq, i) => {
+    faq.addEventListener("click", (e) => {
+      faqs_item[i].classList.toggle("open");
+    });
   });
-});
+
+  faqsBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (btn.classList.contains("active")) return;
+      faqsBtns.forEach((b) => b.classList.toggle("active"));
+      faqsBtnsWrapper.classList.toggle("active");
+      containers.forEach((contain) => contain.classList.toggle("active"));
+    });
+  });
+};
