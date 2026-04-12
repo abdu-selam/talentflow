512d4fd5423bb2a6ed671a43f141f8b246525ba1
Abduselam Awel <abdselam676@gmail.com>
2026-04-11 21:14:35 -0900

    portfolio page has been completed

---
+++ scripts/freelancerPortfolio.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+  const loading = document.querySelector(".loading");
+
+  loading.classList.add("close");
+  init();
+  setTimeout(() => {
+    loading.style.display = "none";
+  }, 500);
+});
+
+const init = () => {
+  document.querySelector("#app").style.display = "flex";
+
+  const navBar = document.querySelector(".aside__nav");
+  const menu = document.querySelector(".menu");
+  const menuIcons = document.querySelectorAll(".menu-icon");
+  const navItems = navBar.querySelectorAll("ul, li,a");
+
+  menu.addEventListener("click", (e) => {
+    menu.classList.toggle("active");
+    navBar.classList.toggle("active");
+  });
+
+  window.addEventListener("click", (e) => {
+    const elem = e.target;
+    if (![...navItems, ...menuIcons, menu, navBar].includes(elem)) {
+      menu.classList.remove("active");
+      navBar.classList.remove("active");
+    }
+  });
+
+  window.addEventListener("scroll", () => {
+    menu.classList.remove("active");
+    navBar.classList.remove("active");
+  });
+};
