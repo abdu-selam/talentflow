a422056f9fe09b79b2b5468ee9e3f9dfc52471b1
Abduselam Awel <abdselam676@gmail.com>
2026-04-20 10:40:32 -0900

    users model has been added

---
+++ api/models/users.php
@@ -0,0 +1,72 @@
+<?php
+class Users
+{
+    private $con;
+    private $table = "users";
+
+    public function __construct($con)
+    {
+        $this->con = $con;
+    }
+
+    public function create($fname, $lname, $email, $password, $roll)
+    {
+        $sql = "INSERT INTO " . $this->table . " (first_name, last_name, email, password, roll) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("sssss", $fname, $lname, $email, $password, $roll);
+
+        return $stmt->execute();
+    }
+
+    public function get_user_by_id($id)
+    {
+        $sql = "SELECT * FROM " . $this->table . " WHERE id = ?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("s", $id);
+
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function get_user_by_email($email)
+    {
+        $sql = "SELECT * FROM " . $this->table . " WHERE email = ?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("s", $email);
+
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function get_user_by_username($user_name)
+    {
+        $sql = "SELECT * FROM " . $this->table . " WHERE user_name = ?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("s", $user_name);
+
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function get_users()
+    {
+
+        $sql = "SELECT * FROM " . $this->table;
+        $result = $this->con->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function get_users_by_roll($roll)
+    {
+        $sql = "SELECT * FROM " . $this->table . " WHERE roll = ?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("s", $roll);
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+}
+
+?>
