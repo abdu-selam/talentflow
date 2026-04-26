76185972fda4522262ed1850f58e8ae63c7bb957
Abduselam Awel <abdselam676@gmail.com>
2026-04-26 08:30:15 -0900

    logout with cookie has been added

--- api/auth/logout.php
+++ api/auth/logout.php
@@ -1,8 +1,12 @@
 <?php
 require_once "../utils/responce.php";
 require_once "../index.php";
+require_once "../utils/cookie.php";
 
     session_destroy();
+    if (isset($_COOKIE["user"])) {
+        cookie_deleter();
+    }
 
     $data = [
         "status" => "success",
--- api/utils/cookie.php
+++ api/utils/cookie.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ ."/env.php";
+require_once __DIR__ . "/env.php";
 
 function cookie_setter($value)
 {
@@ -7,6 +7,19 @@ function cookie_setter($value)
 
     setcookie("user", $value, [
         "expires" => time() + (60 * 60 * 24 * 15),
+        "path" => "/",
+        "secure" => $env["DEV_ENV"] == "production",
+        "httponly" => true,
+        "samesite" => "Strict"
+    ]);
+}
+
+function cookie_deleter()
+{
+    global $env;
+
+    setcookie("user", "", [
+        "expires" => time() - 100000,
         "path" => "/",
         "secure" => $env["DEV_ENV"] == "production",
         "httponly" => true,
